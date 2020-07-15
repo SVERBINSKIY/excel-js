e56352459f90c149ee42d9a67429247783747882
SVERBINSKIY <vladimir.sverbinskiy@gmail.com>
2020-07-15 19:58:15 +0300

    finish state app

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -5,37 +5,45 @@ export class ExcelComponent extends DomListener {
     super($root, options.listeners)
     this.name = options.name || ''
     this.emitter = options.emitter
+    this.subscribe = options.subscribe || []
+    this.store = options.store
     this.unsubscribers = []
+    this.storeSub = null
     this.prepare()
   }
 
   // Настраивает наш компанент до init
   prepare() {}
-
   // Возвращает шаблон компонента
   toHTML() {
     return ''
   }
-
   // Уведомляет слушателей про событие Event
   $emit(event, ...args) {
     const unsub = this.emitter.emit(event, ...args)
     this.unsubscribers.push(unsub)
   }
-
   // Подписывает на событие
   $on(event, fn) {
     this.emitter.subscribe(event, fn)
   }
 
+  $dispatch(action) {
+    this.store.dispatch(action)
+  }
   // Инициализирует компонент, добавляем DOM слушателей
   init() {
     this.initDOMListeners()
   }
-
   // Удаляет компонент, Чистит слушателей
   desrtoy() {
     this.removeDOMListeners()
     this.unsubscribers.forEach(unsub => unsub())
+  }
+  // Приходят только изменения по тем полям, на которые мы подписались.
+  storeChanges() {}
+
+  isWatching(key) {
+    return this.subscribe.includes(key)
   }
 }
